8ca98259f63bd30bcbf97d8c578f913ad488ec2c
Juan-Ajmat <119417051+Juan-Ajmat@users.noreply.github.com>
2023-06-02 09:52:45 -0300

    Update calculadora.c

--- src/calculadora.c
+++ src/calculadora.c
@@ -47,91 +47,15 @@
 
 /* === Private data type declarations ========================================================== */
 
-typedef struct operacion_s * operacion_t;
-
-struct calculadora_s {
-    operacion_t operaciones;
-};
-
-struct operacion_s {
-    char operador;
-    funcion_t funcion;
-    operacion_t siguiente;
-};
-
 /* === Private variable declarations =========================================================== */
 
 /* === Private function declarations =========================================================== */
-
-
-operacion_t BuscarOperacion(calculadora_t calculadora, char operador);
 
 /* === Public variable definitions ============================================================= */
 
 /* === Private variable definitions ============================================================ */
 
 /* === Private function implementation ========================================================= */
-
-calculadora_t CrearCalculadora(void) {
-    calculadora_t calculadora = malloc(sizeof(struct calculadora_s));
-    if (calculadora) {
-        memset(calculadora, 0, sizeof(struct calculadora_s));
-    }
-    return calculadora;
-}
-
-bool AgregarOperacion(calculadora_t calculadora, char operador, funcion_t funcion) {
-    operacion_t operacion = NULL;
-
-    if (!BuscarOperacion(calculadora, operador)) {
-        operacion = malloc(sizeof(struct operacion_s));
-    }
-    if (operacion) {
-        operacion->operador = operador;
-        operacion->funcion = funcion;
-        operacion->siguiente = calculadora->operaciones;
-        calculadora->operaciones = operacion;
-    }
-    return (operacion != NULL);
-}
-
-operacion_t BuscarOperacion(calculadora_t calculadora, char operador) {
-    operacion_t result = NULL;
-    operacion_t actual = calculadora->operaciones;
-
-    if (actual != NULL) {
-        for (actual; actual != NULL; actual = actual->siguiente) {
-            if (actual->operador == operador) {
-                result = actual;
-                break;
-            }
-        }
-    }
-    return result;
-}
-
-int Calcular(calculadora_t calculadora, char * cadena) {
-    int a, b;
-    char operador;
-    int resultado = 0;
-    operacion_t operacion = NULL;
-
-    for (int index = 0; index < strlen(cadena); index++) {
-        if (cadena[index] < '0') {
-            operador = cadena[index];
-            a = atoi(cadena);
-            b = atoi(cadena + index + 1);
-            break;
-        }
-    }
-
-    operacion = BuscarOperacion(calculadora, operador);
-    if (operacion) {
-        resultado = operacion->funcion(a, b);
-    }
-
-    return resultado;
-}
 
 /* === Public function implementation ========================================================== */
 
